2ea50c9d20cb55285fd7766672836c98b82a7f7d
nae-stormrage <stas.markus.2004@mail.ru>
2024-12-13 21:47:22 +0300

    Добавьте файлы проекта.

---
+++ Lab_11/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    class Person
+    {
+        String ID { get; set; }
+        String FirstName { get; set; }
+        String LastName { get; set; }
+        public string email { get; set; }
+        public float Plus { get; set; }
+        public float Minus { get; set; }
+        public long credit_card { get; set; }
+        public float Tax { get; set; }
+
+        public static Person Create (string str)
+        {
+            Person person = new Person ();
+            string[] e = str.Split (',');
+            person.ID = e[0].Trim();
+            person.FirstName = e[1].Trim();
+            person.LastName = e[2].Trim();
+            person.email = e[3].Trim();
+            person.Plus = Convert.ToSingle(e[4].TrimStart('$').Replace('.', ','));
+            person.Minus = Convert.ToSingle(e[5].TrimStart('$').Replace('.', ','));
+            person.credit_card = Convert.ToInt64(e[6]);
+            person.Tax = Convert.ToSingle(e[7].TrimStart('$').Replace('.', ','));
+
+            return person;
+        }
+
+        public override string ToString()
+        {
+            String s = string.Format("***********************************************************************************************\n" + 
+                "ID: {0}, Имя: {1}, Фамилия: {2}, Email: {3}\n" +
+                "Доход: {4}, Расход: {5}, Номер кредитной карты: {6}, Налог: {7}",
+                ID, FirstName, LastName, email,
+                Plus, Minus, credit_card, Tax);
+
+            return s;
+        }
+    }
+}
---
+++ Lab_11/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Net;
+
+namespace Lab_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader f_in = new StreamReader("lr11_10.csv");
+
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            var new_out = new StreamWriter(@"lr11_10_output.txt");
+            Console.SetOut(new_out);
+#endif
+
+            List<Person> all = new List<Person>();
+            try
+            {
+                String line = f_in.ReadLine();
+                while ((line = f_in.ReadLine()) != null)
+                {
+                    all.Add(Person.Create(line));
+                }
+            } catch (Exception ex) 
+            { 
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Всего клиентов банка: {0}", all.Count);
+
+            Console.WriteLine("\n*****Задача 1*****");
+            float client_otricatel_balance = (from p in all where p.Plus - p.Minus < 0 select p).Count();
+            Console.WriteLine("Кол-во клиентов с отрицательным балансом счета: {0}", client_otricatel_balance);
+
+            Console.WriteLine("\n*****Задача 2*****");
+            float average_tax = (from p in all select p.Tax).Sum()/all.Count;
+            Console.WriteLine("Средний налог по всем клиентам: {0}", average_tax);
+
+            Console.WriteLine("\n*****Задача 3*****");
+            float max_minus = (from p in all select p.Minus).Max();
+            Person client_max_minus = (from p in all where (p.Minus == max_minus) select p).First();
+            Console.WriteLine("Клиент с максимальным расходом:\n{0}", client_max_minus);
+
+            Console.WriteLine("\n*****Задача 4*****");
+            float average_plus_without_email = (from p in all where p.email == "" select p.Plus).Sum() / (from p in all where p.email == "" select p).Count();
+            Console.WriteLine("Средний доход клиентов без email: {0}", average_plus_without_email);
+
+#if !DEBUG
+            Console.SetOut(save_out);
+            new_out.Close();
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
